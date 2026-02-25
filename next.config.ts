f56880f919a3324777f9904c13d3dbead459d107
“tinkibelll” <tanilee44@gmail.com>
2026-02-24 19:33:24 -0500

    yey

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/sandbox' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/sandbox' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
