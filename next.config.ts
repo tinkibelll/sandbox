a4a3619d1b8f9dd90d935afe5c86b1cb74b05ebd
“tinkibelll” <tanilee44@gmail.com>
2026-02-24 19:21:45 -0500

    Fix.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'export',
-};
+  basePath: '/sandbox',
+  assetPrefix: '/sandbox',
+}
 
 export default nextConfig;
