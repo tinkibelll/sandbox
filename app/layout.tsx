b5068c61f3d13406474a8ad5979c195d68dc9745
“tinkibelll” <tanilee44@gmail.com>
2026-02-26 11:01:30 -0500

    GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Nunito } from "next/font/google";
+import Script from "next/script";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -14,7 +15,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={nunito.className}>
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-LDEGMPN6R0`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LDEGMPN6R0');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
